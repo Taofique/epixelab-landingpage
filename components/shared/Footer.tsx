7966c17e98c5b11bff6f8a9b66a63c367c8fe8c4
Taofique <shornob8@gmail.com>
2026-08-27 22:31:06 +0600

    feat: Complete initial footer UI design

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router";
+import FooterLogo from "../../app/assets/footer-logo.png";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="container mx-auto flex items-center justify-between">
+      <div className="flex items-center justify-between gap-23 py-4">
+        <img src={FooterLogo} alt="EpixELab Logo" />
+        <p> &copy; 2026 Epixelab. All rights reserved.</p>
+      </div>
+
+      <div className="space-x-6">
+        <Link to="#">Portfolio</Link>
+        <Link to="#">How it Works</Link>
+        <Link to="#">Pricing</Link>
+        <Link to="#">About</Link>
+        <Link to="#">Login</Link>
+      </div>
+    </div>
+  );
 }
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -4,13 +4,14 @@ import Logo from "../../app/assets/Logo.svg";
 export default function Navbar() {
   return (
     <nav className="container mx-auto flex items-center justify-between py-4">
-      <img src={Logo} alt="EpixELab Logo" />
-
-      <div className="space-x-10">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/our-app">Our App</Link>
-        <Link to="/contact">Home</Link>
+      <div className="flex items-center justify-between gap-60 ">
+        <img src={Logo} alt="EpixELab Logo" />
+        <div className="space-x-10">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/our-app">Our App</Link>
+          <Link to="/contact">Home</Link>
+        </div>
       </div>
 
       <div className="space-x-10">

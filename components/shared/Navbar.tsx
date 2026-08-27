0edf58e37dbb043d0d97089bf2819604d8683cb9
Taofique <shornob8@gmail.com>
2026-08-27 20:06:37 +0600

    feat: Complete navbar UI design

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -3,8 +3,22 @@ import Logo from "../../app/assets/Logo.svg";
 
 export default function Navbar() {
   return (
-    <nav className="container mx-auto">
+    <nav className="container mx-auto flex items-center justify-between py-4">
       <img src={Logo} alt="EpixELab Logo" />
+
+      <div className="space-x-10">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/our-app">Our App</Link>
+        <Link to="/contact">Home</Link>
+      </div>
+
+      <div className="space-x-10">
+        <button className="text-lg">Sign In</button>
+        <button className="text-lg bg-[#FF6600] px-4 py-2 rounded-lg text-white">
+          Sign Up
+        </button>
+      </div>
     </nav>
   );
 }
